ae50538a44493b8a477affa4bc2ea6ef38ba6106
yashd <yashwanthduvvuru05@gmail.com>
2019-04-27 11:56:50 -0400

    Code refactoring

--- state_calc.py
+++ state_calc.py
@@ -20,8 +20,6 @@ def completedata():
 
 def prepare_state_df():
     df = pd.read_csv(filename)
-    # df = df.head(100)
-    # state_df=df[['case_no','employer_state','foreign_worker_info_state','job_info_work_state','case_status']]
     state_df = df[['case_no', 'employer_state', 'case_status']]
     state_df = state_df.dropna();
     state_values = state_df['employer_state'].values;
@@ -32,20 +30,14 @@ def prepare_state_df():
             new_state_values.append(state_mapping.state_map[val].upper());
         else:
             new_state_values.append(val.upper());
+
     state_df['new_state'] = new_state_values
     state_df['state_code'] = [state_mapping.stateCodes[x] if x in state_mapping.stateCodes else "" for x in
                               new_state_values];
 
-    # grouped_df=state_df.groupby(['new_state'])['case_status'].value_counts()
-    # print(new_state_values)
     grouped_df = state_df.groupby(['new_state','state_code', 'case_status']).size().unstack(fill_value=0)
-    # print(grouped_df.columns)
-    #
     final_df = pd.DataFrame(grouped_df.reset_index().values,
                             columns=["State", 'state_code',"Certified", "Certified-Expired", "Denied", "Withdrawn"])
-    # print(df.to_json(orient='records'))
-    # return render_template('statemap.html', html=final_df.to_html())
-    # return pd.json.dumps(final_df);
     return final_df;
 
 
@@ -54,6 +46,5 @@ def get_state_decision_data():
     return state_df.to_json(orient="records");
 
 if __name__ == '__main__':
-
     state_df=prepare_state_df();
     app.run("localhost", 8080, debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -28,11 +28,9 @@ function draw_us_chart(data,title){
 
 
     console.log("Logging title:",title);
-    console.log("Logging result:",data);
 
     var data=JSON.parse(data);
-    console.log("Logging result:",data);
-
+    //console.log("Logging result:",data);
 
     var min_val=d3.min(data, function(d){return +d.Certified;})
     var max_val=d3.max(data, function(d){return +d.Certified;})
@@ -50,27 +48,15 @@ function draw_us_chart(data,title){
 		    .append('g')
 			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-
-//    var color=d3.scaleThreshold().domain([0,55]).range(["#1f77b4","#17becf"]);
-    var color = d3.scaleLinear().domain([min_val, max_val]).range(['beige', 'red']);
-
-//    var color = d3.scaleThreshold()
-//            .domain(d3.range(2, 50).map(function(d) { return d * 1000; }))
-//            .range(d3.schemeBlues[9]);
-
-
     var color = d3.scaleQuantile()
 	      .domain([d3.min(data, function(d) { return +d.Certified; }), d3.max(data, function(d) { return +d.Certified; })])
 	      .range(['#deebf7','#c6dbef','#AFE4FD','#9DE1FF','#AEDFF2','#9ecae1','#6baed6','#54CBFF','#42C0FB','#0BB5FF','#2171b5','#08519c','#08306b']);
 
-
     var final_data = d3.map();
-  data.forEach(function(d) {
-    final_data.set(+d.state_code, { State:d.State,Certified: +d.Certified });
-  });
+    data.forEach(function(d) {
+            final_data.set(+d.state_code, { State:d.State,Certified: +d.Certified });
+    });
 
-    console.log("final data:");
-    console.log(final_data)
     var path=d3.geoPath();
 
     var text = svg.append('text')
@@ -87,75 +73,39 @@ function draw_us_chart(data,title){
     .attr("class", "key")
     .attr("transform", "translate(0,0)")
 
-
-
     //d3.json("https://unpkg.com/us-atlas@1/us/10m.json",function(error,us){
     d3.json("https://d3js.org/us-10m.v1.json",function(error,us){
         if(error) throw error;
 
         var project=d3.geoIdentity().fitSize([width,height],topojson.feature(us,us.objects.states));
-        //var path=d3.geoPath().projection(project);
 
-//        svg.append("g")
-//            .attr("class","state")
-//            .selectAll("path")
-//            .data(topojson.feature(us,us.objects.states).features)
-//            .enter().append("path")
-            //.attr("fill",function(d) { console.log("Inside color map:",d.id,+d.id);return color(+d.id);});
+        var tip_disp = d3.tip().attr('class', 'd3-tip').offset([-5, 0])
+        svg.call(tip_disp);
 
+        var states = topojson.feature(us, us.objects.states).features;
 
-//        svg.append("path")
-//            .datum(topojson.mesh(us,us.objects.states,function(a,b){return a!==b;}))
-//            .attr("class","states")
-//            //.attr('fill','none')
-//            .attr("d",path)
+        svg.append("path")
+            .attr("id", "nation")
+            .attr("d", path(topojson.feature(us, us.objects.nation)));
 
+        svg.append("path")
+            .attr("fill", function(d){console.log("Inside mesh fill:",d)})
+            .attr("stroke", "#777")
+            .attr("stroke-width", 0.70)
+            .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })))
 
-//          svg.append("path")
-//      .attr("fill", "none")
-//      .attr("stroke", "#777")
-//      .attr("stroke-width", 0.35)
-//      .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { return (a.id / 1000 | 0) === (b.id / 1000 | 0); })));
-
-
-
-//console.log('here is how the data map works', data);
-
-   var tip_disp = d3.tip().attr('class', 'd3-tip').offset([-5, 0])
-   svg.call(tip_disp);
-
-  var states = topojson.feature(us, us.objects.states).features;
-
-    svg.append("path")
-      .attr("id", "nation")
-      .attr("d", path(topojson.feature(us, us.objects.nation)));
-
-    svg.append("path")
-      .attr("fill", function(d){console.log("Inside mesh fill:",d)})
-      .attr("stroke", "#777")
-      .attr("stroke-width", 0.70)
-      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })))
-
-  svg.selectAll('path.state')
-      .data( states )
-    .enter().append("path")
-      .attr("d", path)
-      .attr("stroke", "#aaa")
-      .attr("stroke-width", 0.5)
-      .attr("fill",function(d){ console.log("Inside fill:",+d.id);return color(final_data.get(+d.id).Certified)})
-      .on('mouseover', function(d) {
-        var name = d3.select(this);
-		console.log("name:"+name);
-		tip_disp.html( "<div style='color:blue,font-weight: bold,font-size: 150%;'>" +final_data.get(+d.id).State + ", " + final_data.get(+d.id).Certified + "</div>");
-        tip_disp.show(this);
-        //d3.select("#info").html(+d.id+", "+final_data.get(+d.id).State + ", " + final_data.get(+d.id).Certified )
-      })
-
-//https://www2.census.gov/geo/docs/reference/state.txt
-
-
-
-
+        svg.selectAll('path.state')
+            .data( states )
+            .enter().append("path")
+            .attr("d", path)
+            .attr("stroke", "#aaa")
+            .attr("stroke-width", 0.5)
+            .attr("fill",function(d){ console.log("Inside fill:",+d.id);return color(final_data.get(+d.id).Certified)})
+            .on('mouseover', function(d) {
+                tip_disp.html( "<div style='color:blue,font-weight: bold,font-size: 150%;'>" +final_data.get(+d.id).State + ", " + final_data.get(+d.id).Certified + "</div>");
+                tip_disp.show(this);
+                //d3.select("#info").html(+d.id+", "+final_data.get(+d.id).State + ", " + final_data.get(+d.id).Certified )
+            })
 
     });
 }
